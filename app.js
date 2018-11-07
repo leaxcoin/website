44e568d962bb19ccd3e992202f9d0b8344159261
Marcos Kubis <marcoskubis@gmail.com>
2018-11-07 20:53:47 -0200

    Set app to listen on port defined in env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sessionService = require('./services/sesssion.service');
 const controllers = require('./controllers/index');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 sessionService(app);
