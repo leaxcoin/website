0d8096c2d24456794fdad7f07f71a99fe94f0d11
Marcos Kubis <marcoskubis@gmail.com>
2018-11-09 20:39:03 -0200

    Add multi language support

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,6 @@ const langService = require('../services/lang.service');
 
 module.exports = app => {
 
-    app.get('/gist', (req, res) => {
-        res.render('gist');
-    });
-
     app.get('/data', langService.middleware, (req, res) => {
         let lang = req.session.savedLang;
         let data = langService.getData(lang);
--- services/template.service.js
+++ services/template.service.js
@@ -1,8 +1,6 @@
-const mustacheExpress = require('mustache-express');
 const VIEWS_PATH = __dirname + '/../views';
 
 module.exports = app => {
-    app.engine('mst', mustacheExpress());
-    app.set('view engine', 'mst');
+    app.set('view engine', 'pug')
     app.set('views', VIEWS_PATH);
 }
